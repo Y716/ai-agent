181deb851a4805ca79dde60a252225a64d583dff
Yasin Hakim <ibnkhaqim@gmail.com>
2026-01-01 17:03:33 +0700

    Refactor to clean code

--- main.py
+++ main.py
@@ -1,84 +1,85 @@
 import os
+import argparse
 from google import genai
 from google.genai import types
 from dotenv import load_dotenv
 import sys
 from available_functions import available_functions
 from call_function import call_function
+from prompts import system_prompt
+from config import MAX_ITERS
 
-args = sys.argv[1:]
+def main():
+    parser = argparse.ArgumentParser(description="AI Coding Assistant")
+    parser.add_argument("user_prompt", type=str, help="Prompt to send to Gemini")
+    parser.add_argument("--verbose", action="store_true", help="Activate Verbose Ouput (more words)")
+    args = parser.parse_args()
 
-if not args:
-    print("Must provide Prompt")
-    exit(1)
-user_prompt = " ".join(args)
-messages = [
-    types.Content(role="user", parts=[types.Part(text=user_prompt)])
-]
-system_prompt = """
-    You are a helpful AI coding agent.
+    if not args:
+        print("Must provide Prompt")
+        exit(1)
 
-    When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    if not api_key:
+        raise RuntimeError("GEMINI_API_KEY environment variable not set")
 
-    - List files and directories
-    - Read file contents
-    - Execute Python files with optional arguments
-    - Write or overwrite files
+    client = genai.Client(api_key=api_key)
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=args.user_prompt)])
+    ]
+    if args.verbose:
+        print(f"User prompt: {args.user_prompt}\n")
 
-    All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
-    """
+    for _ in range(MAX_ITERS):
+        try:
+            final_response = generate_content(client, messages, args.verbose)
+            if final_response:
+                print("Final Response: ")
+                print(final_response)
+                return
+        except Exception as e:
+            print(f"Error Encounter: {e}")
+            exit()
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
+    print(f"Max Iterations has reached: {MAX_ITERS} iterations")
+    sys.exit(1)
 
-client = genai.Client(api_key=api_key)
-is_verbose = "--verbose" in args
+def generate_content(client, messages, verbose):
+    response = client.models.generate_content(
+                model="gemini-2.5-flash",
+                contents= messages,
+                config=types.GenerateContentConfig(
+                    tools=[available_functions], system_instruction=system_prompt)
+            )
+    if not response.usage_metadata:
+        raise RuntimeError("Gemini API response appears to be malformed")   
+    if verbose:
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
-for _ in range(20):
-    try:
-        response = client.models.generate_content(
-            model="gemini-2.5-flash",
-            contents= messages,
-            config=types.GenerateContentConfig(
-                tools=[available_functions], system_instruction=system_prompt)
-        )
-        if response.candidates is not None:
-            for candidate in response.candidates:
-                if candidate.content:
-                    messages.append(candidate.content)  
-    except Exception as e:
-        print(f"Error Encounter: {e}")
-        exit()
+    if response.candidates:
+        for candidate in response.candidates:
+            if candidate.content:
+                messages.append(candidate.content)
+    
+    if response.function_calls:
+        return response.text
 
     function_call_responses  = []
 
     if response.function_calls:
         for function_call in response.function_calls:
-            result = call_function(function_call, verbose=is_verbose)
-            if len(result.parts) != 0:
-                if result.parts[0].function_response != None:
-                    if result.parts[0].function_response.response != None:
-                        function_call_responses.append(result.parts[0])
-                        
-                        if is_verbose:
-                            print(f"-> {result.parts[0].function_response.response}")
-                    else:
-                        raise Exception("function_call_result doesn't return '.parts[0].function_response.response'")
-                else:
-                    raise Exception("function_call_result doesn't return '.parts[0].function_response'")
-            else:
-                raise Exception("function_call_result doesn't return '.parts'")
-    else:
-        print(response.text)
-        break
+            result = call_function(function_call, verbose=verbose)
+            if(len(result.parts != 0
+                   or result.parts[0].function_response != None
+                   or result.parts[0].function_response.response != None)):
+                raise RuntimeError(f"Empty function call response for: {function_call}")
+            
+            if verbose:
+                print(f"-> {result.parts[0].function_response.response}")
+            function_call_responses.append(result.parts[0])
     messages.append(types.Content(role="user", parts=function_call_responses))
 
-    if is_verbose:
-        print(f"User prompt: {user_prompt}")
-        if response.usage_metadata:
-            print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-            print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-
-if response.function_calls:
-    print("It does not give the final answer after 20 iterations, need more iterations")
-    exit(1)
+if __name__ == "__main__":
+    main()
